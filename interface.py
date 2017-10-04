262d3b733a7a12321d898958aeb1dea023dc6bff
lurke <lurke@sunrisecourt.net>
2017-10-04 11:28:54 -0400

    basic flask app that surfaces pull requests over get api call

--- interface.py
+++ interface.py
@@ -2,8 +2,14 @@ import os
 import requests
 import pickle
 import urllib.parse as urlparse
+from flask import Flask, json, abort
+from flask import request as flask_request
 
-def get_pull_requests() :
+app = Flask(__name__)
+CACHE_FILENAME = os.path.abspath(os.path.join(os.path.realpath(__file__), '../cache.pkl'))
+
+
+def cache_pull_requests():
     # set up query
     url = 'https://api.github.com/repos/lodash/lodash/pulls'
     params = {'state': 'all'}
@@ -25,15 +31,33 @@ def get_pull_requests() :
     #     page += 1
 
     # cache in case we exceed github's rate limit
-    cache_filename = os.path.abspath(os.path.join(os.path.realpath(__file__), '../cache.pkl'))
-    # with open(cache_filename, 'wb') as cache_file:
+    # with open(CACHE_FILENAME, 'wb') as cache_file:
     #     pickle.dump(pulls, cache_file)
+
+
+@app.route("/pulls/")
+def get_pull_requests() :
+    start_index = flask_request.args.get('start')
+    end_index = flask_request.args.get('end')
+
+    # handle start and end params
+    if not start_index: start_index = 0
+    if not end_index: end_index = 30
+    if not start_index >= 0:
+        abort(400, "Start param must be positive.")
+    if not end_index >= 0:
+        abort(400, "End param must be positive.")
+    if end_index < start_index:
+        abort(400, "End param must be greater than start param.")
+    if end_index - start_index > 30:
+        abort(400, "You may only request a maximum of 30 requests at a time.")
+
     pulls = None
-    with open(cache_filename, 'rb') as cache_file:
+    with open(CACHE_FILENAME, 'rb') as cache_file:
         pulls = pickle.load(cache_file)
 
-    # compute metrics on pulls, if desired
-    # TODO
+    return json.jsonify(pulls[start_index:end_index])
 
 if __name__ == '__main__':
-    get_pull_requests()
+    cache_pull_requests()
+    app.run()
